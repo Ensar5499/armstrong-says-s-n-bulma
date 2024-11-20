56b942511bcd088a0e032a67dc3b66e0ce67af33
Ensar5499 <ensarkulaksiz54@gmail.com>
2024-11-20 16:41:17 +0300

    Add files via upload
    Girilen sayının armstrong sayısı olup olmadını söyler

---
+++ armstrong sayısı bulma.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        
+           
+            {
+                Console.WriteLine("Bir sayı giriniz:");
+                int n = int.Parse(Console.ReadLine()); 
+
+                int toplam = 0;
+                int orijinalSayi = n;  
+                
+                while (n > 0)
+                {
+                    int basamak = n % 10;  
+                    toplam += basamak * basamak * basamak;  
+                    n /= 10; 
+                }
+
+              
+                if (toplam == orijinalSayi)
+                {
+                    Console.WriteLine("{0} bir Armstrong sayısıdır.", orijinalSayi);
+                }
+                else
+                {
+                    Console.WriteLine("{0} bir Armstrong sayısı değildir.", orijinalSayi);
+                }
+
+                Console.ReadLine();  
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+}
+    
+
